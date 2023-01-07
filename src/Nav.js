541deb5fcfadc61c8e62704453864db0057a0b13
Afolabi Olajide Samuel <samolajide20@gmail.com>
2023-01-07 21:27:09 +0100

    Initial setup

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div className='bg-slate-200'>
+            <div className="head p-6">
+                <h2 className='text-2xl text-gray-800 font-bold'>Business Plan</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
