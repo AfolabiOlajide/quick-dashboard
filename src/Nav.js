aae14b2dcb6820c8b1447fe32ff67022e1bf0384
Afolabi Olajide Samuel <samolajide20@gmail.com>
2023-01-09 12:18:44 +0100

    updated responsiveness and started work (1/9/2023)

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AiFillHome, AiOutlineClose } from "react-icons/ai";
+import { AiFillHome, } from "react-icons/ai";
 import { TiArrowBack } from "react-icons/ti";
 import { TbArrowsRightLeft } from "react-icons/tb";
 import { RiWalletFill } from "react-icons/ri";
@@ -7,36 +7,36 @@ import { FaChartLine } from "react-icons/fa";
 import { IoIosNotifications } from "react-icons/io";
 
 
-const Nav = ({ isActive,changeActiveState }) => {
-    const onChangeActiveState = () => {
-        changeActiveState();
-    }
+const Nav = ({ isActive, }) => {
+    // const onChangeActiveState = () => {
+    //     changeActiveState();
+    // }
 
     return (
-        <div className={`bg-slate-200 fixed top-0 ${isActive ? "left-0" : "-left-[130%]"} bottom-0 w-[50vw] md:left-0 md:relative md:w-[100%] transition-all duration-500 ease-in-out`}>
+        <div className={`bg-slate-200 fixed top-0 ${isActive ? "left-0" : "-left-[130%]"} bottom-0 w-[70vw] md:left-0 md:relative md:w-[100%] transition-all duration-500 ease-in-out`}>
             <div className="head p-12">
                 <h2 className='text-2xl text-gray-800 font-bold'>Business Plan</h2>
             </div>
             <div className="menu-list">
                 <div className="menu pl-[3rem] p-3 hover:pl-[4.5rem] transition-all duration-700 ease-in-out cursor-pointer border-l-[.3rem] text-gray-900 border-gray-800">
-                    <h3 className='flex gap-6 items-center text-[1.3rem]'><AiFillHome /> Overview</h3>
+                    <h3 className='flex gap-6 items-center text-[1rem] md:text-[1.3rem]'><AiFillHome /> Overview</h3>
                 </div>
                 <div className="menu pl-[3rem] p-3 text-slate-600 hover:pl-[4.5rem] transition-all duration-700 ease-in-out cursor-pointer">
-                    <h3 className='flex gap-6 items-center text-[1.3rem]'><TiArrowBack /> Revenue</h3>
+                    <h3 className='flex gap-6 items-center text-[1rem] md:text-[1.3rem]'><TiArrowBack /> Revenue</h3>
                 </div>
                 <div className="menu pl-[3rem] p-3 text-slate-600 hover:pl-[4.5rem] transition-all duration-700 ease-in-out cursor-pointer">
-                    <h3 className='flex gap-6 items-center text-[1.3rem]'><TbArrowsRightLeft /> Costs</h3>
+                    <h3 className='flex gap-6 items-center text-[1rem] md:text-[1.3rem]'><TbArrowsRightLeft /> Costs</h3>
                 </div>
                 <div className="menu pl-[3rem] p-3 text-slate-600 hover:pl-[4.5rem] transition-all duration-700 ease-in-out cursor-pointer">
-                    <h3 className='flex gap-6 items-center text-[1.3rem]'><RiWalletFill /> Services</h3>
+                    <h3 className='flex gap-6 items-center text-[1rem] md:text-[1.3rem]'><RiWalletFill /> Services</h3>
                 </div>
                 <div className="menu pl-[3rem] p-3 text-slate-600 hover:pl-[4.5rem] transition-all duration-700 ease-in-out cursor-pointer">
-                    <h3 className='flex gap-6 items-center text-[1.3rem]'><FaChartLine /> New Sales</h3>
+                    <h3 className='flex gap-6 items-center text-[1rem] md:text-[1.3rem]'><FaChartLine /> New Sales</h3>
                 </div>
                 <div className="menu pl-[3rem] p-3 text-slate-600 hover:pl-[4.5rem] transition-all duration-700 ease-in-out cursor-pointer">
-                    <h3 className='flex gap-6 items-center text-[1.3rem]'><IoIosNotifications /> Notifications</h3>
+                    <h3 className='flex gap-6 items-center text-[1rem] md:text-[1.3rem]'><IoIosNotifications /> Notifications</h3>
                 </div>
-                <div className="block md:hidden close absolute top-3 right-3 text-[1.4rem] bg-slate-400 rounded-full p-1" onClick={onChangeActiveState}><AiOutlineClose /></div>
+                {/* <div className="block md:hidden close absolute top-3 right-3 text-[1.4rem] bg-slate-400 rounded-full p-1" onClick={onChangeActiveState}><AiOutlineClose /></div> */}
             </div>
         </div>
     )
--- src/Overview.js
+++ src/Overview.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { GiHamburgerMenu } from "react-icons/gi"
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GoChevronDown } from "react-icons/go";
 
 const Overview = ({ changeActiveState }) => {
 
@@ -8,10 +9,26 @@ const Overview = ({ changeActiveState }) => {
     }
 
     return (
-        <div className='mx-[2rem] my-[3rem]'>
-            <div className="head"><GiHamburgerMenu className='cursor-pointer' onClick={onChangeActiveState} /></div>
+        <div className='mx-[1rem] md:mx-[2rem] my-[3rem]'>
+            <div className="head flex justify-between items-center">
+                <h2 className='text-[1.3rem] md:text-4xl font-bold text-gray-700'>Revenue</h2>
+                <div className="profile flex justify-between items-center gap-7">
+                    <div className="country text-[.7rem] md:text-lg flex items-center gap-2 md:gap-5 font-bold text-gray-700">Sweden <GoChevronDown /></div>
+                    <div className="profile-details flex justify-between items-center gap-5">
+                        <div className="img ">
+                            <img className='object-cover h-[1.5rem] w-[1.5rem] md:h-[3rem] md:w-[3rem] rounded-full' src="https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        </div>
+                        <div className="text-[.7rem] md:text-lg name flex items-center gap-2 md:gap-5 font-bold text-gray-700">Nicola Rich <GoChevronDown /></div>
+                        <div className="menu block md:hidden">
+                            <GiHamburgerMenu className='cursor-pointer' onClick={onChangeActiveState} />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
 
 export default Overview
+
+// <GiHamburgerMenu className='cursor-pointer' onClick={onChangeActiveState} />
